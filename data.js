737ab832b777d39fe0a6a1b317c2b0c2b012c70d
vloneqwer99-hue <vloneqwer99@gmail.com>
2026-03-03 15:25:18 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,136 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360--1-jpg",
+      "name": "360 (Вид 1) JPG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "yaw": -0.5053241487619182,
+        "pitch": 0.011652623863525946,
+        "fov": 1.1960518498450292
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2277626499705967,
+          "pitch": -0.02153536978487125,
+          "rotation": 0,
+          "target": "1-360--2-jpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360--2-jpg",
+      "name": "360 (Вид 2) JPG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4673294357347242,
+          "pitch": -0.0673143223156778,
+          "rotation": 0,
+          "target": "2-360--3-jpg"
+        },
+        {
+          "yaw": -2.420108455420049,
+          "pitch": -0.048185482534005786,
+          "rotation": 0,
+          "target": "0-360--1-jpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-360--3-jpg",
+      "name": "360 (Вид 3) JPG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
